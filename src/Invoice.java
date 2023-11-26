4ea987fd8a568116217a6c02bf4a87aac3d1aaeb
hiphuc179 <147999463+hiphuc179@users.noreply.github.com>
2023-11-26 14:22:52 +0700

    Update Invoice.java

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,19 +1,58 @@
-class Invoice {
-    private List<Sale> sales;
+import java.io.*;
 
-    public Invoice() {
-        sales = new ArrayList<>();
+public class Invoice {
+    private int soLuong;
+    private double donGia;
+
+    public Invoice(int soLuong, double donGia) {
+        this.soLuong = soLuong;
+        this.donGia = donGia;
     }
 
-    public void addSale(Sale sale) {
-        sales.add(sale);
+    public int getSoLuong() {
+        return soLuong;
     }
 
-    public double getTotalAmount() {
-        double total = 0.0;
-        for (Sale sale : sales) {
-            total += sale.getTotalAmount();
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public double getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(double donGia) {
+        this.donGia = donGia;
+    }
+
+    public double tinhTongTien() {
+        return soLuong * donGia;
+    }
+
+
+    public void xuatThongTin(String tenFile) {
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(tenFile));
+            writer.println("Số lượng: " + soLuong);
+            writer.println("Đơn giá: " + donGia);
+            writer.println("Tổng tiền: " + tinhTongTien());
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Lỗi ghi file: " + e.getMessage());
         }
-        return total;
+    }
+
+    public static Invoice docThongTin(String tenFile) {
+        Invoice invoice = null;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(tenFile));
+            int soLuong = Integer.parseInt(reader.readLine().split(": ")[1]);
+            double donGia = Double.parseDouble(reader.readLine().split(": ")[1]);
+            invoice = new Invoice(soLuong, donGia);
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Lỗi đọc file: " + e.getMessage());
+        }
+        return invoice;
     }
 }
