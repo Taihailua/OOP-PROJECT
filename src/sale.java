63af9275a40d3d3c756ae5341b551597b6de6b9b
hiphuc179 <147999463+hiphuc179@users.noreply.github.com>
2023-11-26 14:24:43 +0700

    Update sale.java

--- src/sale.java
+++ src/sale.java
@@ -1,18 +1,71 @@
-class Sale {
-    private int productId;
-    private int quantity;
-    private double price;
+import java.io.*;
 
-    public Sale(int productId, int quantity, double price) {
-        this.productId = productId;
-        this.quantity = quantity;
-        this.price = price;
+public class Sale {
+    private String tenSanPham;
+    private int soLuong;
+    private double giaBan;
+
+    public Sale(String tenSanPham, int soLuong, double giaBan) {
+        this.tenSanPham = tenSanPham;
+        this.soLuong = soLuong;
+        this.giaBan = giaBan;
     }
 
-    // Getters and setters
+    public String getTenSanPham() {
+        return tenSanPham;
+    }
 
-    public double getTotalAmount() {
-        return quantity * price;
+    public void setTenSanPham(String tenSanPham) {
+        this.tenSanPham = tenSanPham;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public double getGiaBan() {
+        return giaBan;
+    }
+
+    public void setGiaBan(double giaBan) {
+        this.giaBan = giaBan;
+    }
+
+    public double tinhTongTien() {
+        return soLuong * giaBan;
+    }
+
+    public void xuatThongTin(String tenFile) {
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(tenFile));
+            writer.println("Tên sản phẩm: " + tenSanPham);
+            writer.println("Số lượng: " + soLuong);
+            writer.println("Giá bán: " + giaBan);
+            writer.println("Tổng tiền: " + tinhTongTien());
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Lỗi ghi file: " + e.getMessage());
+        }
+    }
+
+    //  đọc thông tin đơn hàng từ file
+
+    public static Sale docThongTin(String tenFile) {
+        Sale sale = null;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(tenFile));
+            String tenSanPham = reader.readLine().split(": ")[1];
+            int soLuong = Integer.parseInt(reader.readLine().split(": ")[1]);
+            double giaBan = Double.parseDouble(reader.readLine().split(": ")[1]);
+            sale = new Sale(tenSanPham, soLuong, giaBan);
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Lỗi đọc file: " + e.getMessage());
+        }
+        return sale;
     }
 }
-
