019fda03077f9800315d4ac2b44a4a204667150d
hiphuc179 <147999463+hiphuc179@users.noreply.github.com>
2023-11-28 14:25:32 +0700

    Update Invoice.java

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,58 +1,65 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
 
 public class Invoice {
-    private int soLuong;
-    private double donGia;
+    private Order order;
+    private Date invoiceDate;
+    private Salesperson salesperson;
+    private Sale sale;
+    private Payment payment;
 
-    public Invoice(int soLuong, double donGia) {
-        this.soLuong = soLuong;
-        this.donGia = donGia;
+    public Invoice(Order order, Date invoiceDate, Sale sale, Salesperson salesperson) {
+        this.order = order;
+        this.invoiceDate = invoiceDate;
+        this.sale = sale;
+        this.salesperson = salesperson;
     }
 
-    public int getSoLuong() {
-        return soLuong;
+    public void printInvoice() {
+        System.out.println("Invoice Information:");
+        System.out.println("===============================");
+        System.out.println("Order ID: " + order.getOrderId());
+        System.out.println("Customer: " + order.getCustomerName());
+        System.out.println("Invoice Date: " + invoiceDate);
+        System.out.println("Salesperson: " + salesperson.getName());
+        System.out.println("Total Amount: $" + sale.getTotalAmount());
+        System.out.println("===============================");
     }
 
-    public void setSoLuong(int soLuong) {
-        this.soLuong = soLuong;
-    }
-
-    public double getDonGia() {
-        return donGia;
-    }
-
-    public void setDonGia(double donGia) {
-        this.donGia = donGia;
-    }
-
-    public double tinhTongTien() {
-        return soLuong * donGia;
-    }
-
-
-    public void xuatThongTin(String tenFile) {
+    public void saveToFile(String fileName) {
         try {
-            PrintWriter writer = new PrintWriter(new FileWriter(tenFile));
-            writer.println("Số lượng: " + soLuong);
-            writer.println("Đơn giá: " + donGia);
-            writer.println("Tổng tiền: " + tinhTongTien());
+            FileWriter writer = new FileWriter(fileName);
+            writer.write("Invoice Information:\n");
+            writer.write("===============================\n");
+            writer.write("Order ID: " + order.getOrderId() + "\n");
+            writer.write("Customer: " + order.getCustomerName() + "\n");
+            writer.write("Invoice Date: " + invoiceDate + "\n");
+            writer.write("Salesperson: " + salesperson.getName() + "\n");
+            writer.write("Total Amount: $" + sale.getTotalAmount() + "\n");
+            writer.write("===============================\n");
             writer.close();
+            System.out.println("Hóa đơn đã được lưu vào file: " + fileName);
         } catch (IOException e) {
-            System.out.println("Lỗi ghi file: " + e.getMessage());
+            System.out.println("Có lỗi khi lưu hóa đơn vào file.");
+            e.printStackTrace();
         }
     }
 
-    public static Invoice docThongTin(String tenFile) {
-        Invoice invoice = null;
+    public void readFromFile(String fileName) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(tenFile));
-            int soLuong = Integer.parseInt(reader.readLine().split(": ")[1]);
-            double donGia = Double.parseDouble(reader.readLine().split(": ")[1]);
-            invoice = new Invoice(soLuong, donGia);
-            reader.close();
-        } catch (IOException e) {
-            System.out.println("Lỗi đọc file: " + e.getMessage());
+            File file = new File(fileName);
+            java.util.Scanner scanner = new java.util.Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+           System.out.println("Không tìm thấy file.");
+            e.printStackTrace();
         }
-        return invoice;
     }
 }
