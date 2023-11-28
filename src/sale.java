770dc59d5064af37098ba55f4b5ba21b274e3aad
hiphuc179 <147999463+hiphuc179@users.noreply.github.com>
2023-11-28 14:30:40 +0700

    Update sale.java

--- src/sale.java
+++ src/sale.java
@@ -1,71 +1,27 @@
-import java.io.*;
+import java.util.Date;
 
 public class Sale {
-    private String tenSanPham;
-    private int soLuong;
-    private double giaBan;
+    private String phoneID;
+    private String description;
+    private Date startDate;
+    private Date endDate;
 
-    public Sale(String tenSanPham, int soLuong, double giaBan) {
-        this.tenSanPham = tenSanPham;
-        this.soLuong = soLuong;
-        this.giaBan = giaBan;
+    public Sale(String phoneID, String description, Date startDate, Date endDate) {
+        this.phoneID = phoneID;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 
-    public String getTenSanPham() {
-        return tenSanPham;
+    public void applySale() {
+        // áp dụng khuyến mãi cho sản phẩm
+        System.out.println("Đã áp dụng khuyến mãi cho sản phẩm có ID: " + phoneID);
+        System.out.println("Mô tả: " + description);
+        System.out.println("Thời gian bắt đầu: " + startDate);
+        System.out.println("Thời gian kết thúc: " + endDate);
     }
 
-    public void setTenSanPham(String tenSanPham) {
-        this.tenSanPham = tenSanPham;
+    public void removeSale() {
+        // xoá khuyến mãi 
+        System.out.println("Đã gỡ bỏ khuyến mãi cho sản phẩm có ID: " + phoneID);
     }
-
-    public int getSoLuong() {
-        return soLuong;
-    }
-
-    public void setSoLuong(int soLuong) {
-        this.soLuong = soLuong;
-    }
-
-    public double getGiaBan() {
-        return giaBan;
-    }
-
-    public void setGiaBan(double giaBan) {
-        this.giaBan = giaBan;
-    }
-
-    public double tinhTongTien() {
-        return soLuong * giaBan;
-    }
-
-    public void xuatThongTin(String tenFile) {
-        try {
-            PrintWriter writer = new PrintWriter(new FileWriter(tenFile));
-            writer.println("Tên sản phẩm: " + tenSanPham);
-            writer.println("Số lượng: " + soLuong);
-            writer.println("Giá bán: " + giaBan);
-            writer.println("Tổng tiền: " + tinhTongTien());
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("Lỗi ghi file: " + e.getMessage());
-        }
-    }
-
-    //  đọc thông tin đơn hàng từ file
-
-    public static Sale docThongTin(String tenFile) {
-        Sale sale = null;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(tenFile));
-            String tenSanPham = reader.readLine().split(": ")[1];
-            int soLuong = Integer.parseInt(reader.readLine().split(": ")[1]);
-            double giaBan = Double.parseDouble(reader.readLine().split(": ")[1]);
-            sale = new Sale(tenSanPham, soLuong, giaBan);
-            reader.close();
-        } catch (IOException e) {
-            System.out.println("Lỗi đọc file: " + e.getMessage());
-        }
-        return sale;
-    }
-}
