e8b2f4add4fee0b564f1073a6b6a96532a058325
hiphuc179 <147999463+hiphuc179@users.noreply.github.com>
2023-11-28 14:43:38 +0700

    Update Payment.java

--- src/Payment.java
+++ src/Payment.java
@@ -1,55 +1,23 @@
-import java.io.*;
-
 public class Payment {
-    private String tenKhachHang;
-    private double soTien;
+    private String cardNumber;
+    private String expirationDate;
+    private String cardType;
 
-    public Payment(String tenKhachHang, double soTien) {
-        this.tenKhachHang = tenKhachHang;
-        this.soTien = soTien;
-    }
-    public String getTenKhachHang() {
-        return tenKhachHang;
-    }
-
-    public void setTenKhachHang(String tenKhachHang) {
-        this.tenKhachHang = tenKhachHang;
+    public Payment(String cardNumber, String expirationDate, String cardType) {
+        this.cardNumber = cardNumber;
+        this.expirationDate = expirationDate;
+        this.cardType = cardType;
     }
 
-    public double getSoTien() {
-        return soTien;
+    public String getCardNumber() {
+        return cardNumber;
     }
 
-    public void setSoTien(double soTien) {
-        this.soTien = soTien;
+    public String getExpirationDate() {
+        return expirationDate;
     }
 
-    //  xuất thông tin thanh toán ra file
-
-    public void xuatThongTin(String tenFile) {
-        try {
-            PrintWriter writer = new PrintWriter(new FileWriter(tenFile));
-            writer.println("Tên khách hàng: " + tenKhachHang);
-            writer.println("Số tiền: " + soTien);
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("Lỗi ghi file: " + e.getMessage());
-        }
-    }
-
-    //đọc thông tin thanh toán từ file
-
-    public static Payment docThongTin(String tenFile) {
-        Payment payment = null;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(tenFile));
-            String tenKhachHang = reader.readLine().split(": ")[1];
-            double soTien = Double.parseDouble(reader.readLine().split(": ")[1]);
-            payment = new Payment(tenKhachHang, soTien);
-            reader.close();
-        } catch (IOException e) {
-            System.out.println("Lỗi đọc file: " + e.getMessage());
-        }
-        return payment;
+    public String getCardType() {
+        return cardType;
     }
 }
