330a5c07c93c973a34e4151b46e7f6c6a9c09dde
ThoiThanhVuong <136805692+ThoiThanhVuong@users.noreply.github.com>
2023-11-30 19:55:11 +0700

    Add files via upload

--- src/Sale.java
+++ src/Sale.java
@@ -3,55 +3,13 @@ import java.util.Date;
 public class Sale {
     private String phoneID;
     private String description;
-    private int priceSale;
     private Date startDate;
     private Date endDate;
 
-    public Sale(String phoneID, String description,int priceSale, Date startDate, Date endDate) {
+    public Sale(String phoneID, String description, Date startDate, Date endDate) {
         this.phoneID = phoneID;
         this.description = description;
-        this.priceSale=priceSale;
         this.startDate = startDate;
-        this.endDate = endDate;
-    }
-
-    public String getPhoneID() {
-        return phoneID;
-    }
-
-    public void setPhoneID(String phoneID) {
-        this.phoneID = phoneID;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getPriceSale() {
-        return priceSale;
-    }
-
-    public void setPriceSale(int priceSale) {
-        this.priceSale = priceSale;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
         this.endDate = endDate;
     }
 
@@ -67,4 +25,3 @@ public class Sale {
         // xoá khuyến mãi 
         System.out.println("Đã gỡ bỏ khuyến mãi cho sản phẩm có ID: " + phoneID);
     }
-}
